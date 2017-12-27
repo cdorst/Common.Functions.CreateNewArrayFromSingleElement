4be4d11981c4d7e80b44021bd3e36f1a68203045
Christopher Dorst <chris@dorst.email>
2017-12-27 11:13:16 -0600

    Adding ArrayCreator

---
+++ Common.Functions.CreateNewArrayFromSingleElement/ArrayCreator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Common.Functions.CreateNewArrayFromSingleElement
+{
+    public static class ArrayCreator
+    {
+        public static IEnumerable<T> Array<T>(T firstElement) => new T[] { firstElement };
+    }
+}
